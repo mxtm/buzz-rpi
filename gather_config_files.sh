12f54d2d78da05080569df4c0bfbd4aaa0f4a1f9
Max Tamer-Mahoney <max@mxtm.me>
2020-03-31 23:09:04 +0300

    Update configuration files, organize them as in filesystem

--- gather_config_files.sh
+++ gather_config_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cp /etc/frp/frpc.ini ./
-cp /etc/motion/motion.conf ./
+cp /etc/frp/frpc.ini necessary_configs/etc/frp/
+cp /etc/motion/motion.conf necessary_configs/etc/motion/
+cp /etc/default/motion necessary_configs/etc/default/
