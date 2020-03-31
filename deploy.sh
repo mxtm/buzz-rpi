4d23236e408460cdfe78d3f87bb9ddb7afbb74d5
Max Tamer-Mahoney <max@mxtm.me>
2020-03-31 23:14:35 +0300

    Lay out plan for initial deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
 # TODO: write a deployment script that goes from a fresh RPi Raspbian install to Buzz unit
-# This should include:
-# installing and configuring motion
-# installing and configuring frpc
+# This should go similar to as follows:
+
+# create directory /home/pi/motion
+
+# install motion from Github releases
+
+# copy /etc/default/motion into place
+
+# copy /etc/motion/motion.conf into place
+
+# install frp from Github releases (binary zip for arm), copy to /usr/bin
+
+# copy systemd services into place
+
+# reload systemd daemon
+
+# copy /etc/frp/frpc.ini into place
+
+# install python3 libraries face_recognition and firebase_admin (globally)
+
+# fix permissions
+
+# restart motion systemd service
+
+# enable motion systemd service
+
+# enable frpc systemd service
