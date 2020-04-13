b6fa316bac54bad1ed2570475a615d244af13a05
Max Tamer-Mahoney <max@mxtm.me>
2020-04-13 11:41:46 +0300

    Add a systemd service for the server software

--- gather_config_files.sh
+++ gather_config_files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# TODO: This needs a huge update!
+
 cp /etc/frp/frpc.ini necessary_configs/etc/frp/
 cp /etc/motion/motion.conf necessary_configs/etc/motion/
 cp /etc/default/motion necessary_configs/etc/default/
